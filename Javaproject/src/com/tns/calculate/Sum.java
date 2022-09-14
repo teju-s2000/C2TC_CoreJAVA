a9c3ff8bafbb98371a918c006200aeba330757b4
thejaswini <thejaswini.s24@gmail.com>
2022-09-14 09:41:57 +0530

    Importing package from other program

---
+++ Javaproject/src/com/tns/calculate/Sum.java
@@ -0,0 +1,14 @@
+package com.tns.calculate;
+
+public class Sum {
+	
+	int a = 20;
+	int b = 60;
+	
+	public void cal()
+	{
+		int s = a + b;
+		System.out.println("Sum: "+s);
+	}
+
+}
---
+++ Javaproject/src/com/tns/result/Result.java
@@ -0,0 +1,13 @@
+package com.tns.result;
+
+import com.tns.calculate.*;
+
+public class Result {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Sum s = new Sum();
+      s.cal();
+	}
+
+}
