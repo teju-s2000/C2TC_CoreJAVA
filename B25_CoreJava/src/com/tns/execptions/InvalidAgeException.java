7f97686662170fd95c55c9d2b3a3574d9224f40d
thejaswini <thejaswini.s24@gmail.com>
2022-09-30 14:53:51 +0530

    Exceptions

---
+++ B25_CoreJava/src/com/tns/execptions/InvalidAgeException.java
@@ -0,0 +1,12 @@
+package com.tns.execptions;
+
+
+public class InvalidAgeException extends Exception
+{
+	
+	InvalidAgeException(String str)
+	{
+		super(str);
+	}
+}
+    
---
+++ B25_CoreJava/src/com/tns/execptions/Testclass.java
@@ -0,0 +1,32 @@
+package com.tns.execptions;
+import java.util.Scanner;
+public class Testclass {
+	
+	
+	private static int age;
+	static void validate()throws InvalidAgeException
+	{
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter your age:");
+		
+		age = sc.nextInt();
+		if(age<18)
+			throw new InvalidAgeException("Invalid age,you are not eligible to vote");
+		else
+			System.out.println("Welcome to vote");
+	}
+	public static void main(String[] args) 
+	{
+    try
+    {
+    	validate();
+    }
+    catch(Exception e)
+    {
+    	System.out.println("Caught an Exception:"+e);
+    }
+	}
+
+}
+
+
