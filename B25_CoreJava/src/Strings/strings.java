ec16848394637747ecf1b66e8b638129b77c1c18
thejaswini <thejaswini.s24@gmail.com>
2022-10-19 15:45:20 +0530

    strings

---
+++ B25_CoreJava/src/Strings/strings.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class strings {
+
+	public static void main(String[] args) {
+		
+		String string = "string".replace('i', '0');
+		System.out.println(string.substring(2,5));
+		// TODO Auto-generated method stub
+
+	}
+
+}
